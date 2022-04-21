5a3a54f5be0a462110276bb65abb5ef3faef04c4
aloprui26-06 <102860876+aloprui26-06@users.noreply.github.com>
2022-04-21 11:47:33 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,20 @@
 let izquierda = false
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 20 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 30 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
         izquierda = Math.randomBoolean()
         if (izquierda == true) {
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+            maqueen.motorStop(maqueen.Motors.All)
+            basic.pause(500)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            basic.pause(1000)
+            basic.pause(500)
         }
         if (izquierda == false) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-            basic.pause(1000)
+            basic.pause(500)
         }
     } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     }
 })
