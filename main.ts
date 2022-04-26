8083f2da113aeb718810a07d48660e5406b76ce0
aloprui26-06 <102860876+aloprui26-06@users.noreply.github.com>
2022-04-26 09:07:00 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,18 +3,14 @@ basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 20 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
         izquierda = Math.randomBoolean()
         if (izquierda == true) {
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
-            basic.pause(1000)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            basic.pause(500)
+            basic.pause(1000)
         }
         if (izquierda == false) {
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
             basic.pause(1000)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-            basic.pause(500)
         }
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
